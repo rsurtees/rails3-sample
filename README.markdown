467c5ae2a53ba8eba18def66ea9f54cef26e0c48
Robert Surtees <rsurtees@mac.com>
2011-02-24 14:30:45 -0500

    Initial with improved the README

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Ruby on Rail Tutorial: sample application
+
+This is the sample application for
+[*Ruby on Rails Tutorial: Learn Railsby Example*]9http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'de6c5dd11781280db5152f260078c8b27b5039bd6d8b8b45b33530164c0b4dc9d47e44b7ee7962eaa989e8a44fcbba85b0858322053be572757aeffeb656d3da'
